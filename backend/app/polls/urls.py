f39b620a02daa59d0ff6022c93a59459ff6ab56e
鲨鱼辣椒 <monishine_code@163.com>
2025-05-16 13:39:17 +0800

    feat: 添加视图

--- backend/app/polls/urls.py
+++ backend/app/polls/urls.py
@@ -1,3 +1,4 @@
+from os import name
 from django.urls import path
 
 from . import views
@@ -5,4 +6,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
