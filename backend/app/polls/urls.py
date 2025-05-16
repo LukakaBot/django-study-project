8847ce03f0d13139da900d9e6e8be9986ba1c1f9
鲨鱼辣椒 <monishine_code@163.com>
2025-05-16 15:53:15 +0800

    feat: polls应用添加增删改查接口

--- backend/app/polls/urls.py
+++ backend/app/polls/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
     path("<int:question_id>/", views.detail, name="detail"),
     path("<int:question_id>/results/", views.results, name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
+    path("create", views.create, name="create"),
+    path("get", views.get_poll, name="get_poll"),
+    path("getList", views.get_poll_list, name="get_poll_list"),
+    path("update", views.update_poll, name="update_poll"),
+    path("delete", views.delete_poll, name="delete_poll"),
 ]
--- backend/app/polls/views.py
+++ backend/app/polls/views.py
@@ -1,9 +1,18 @@
+import json
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from .models import Question
+from django.utils import timezone
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
 def index(request):
+    if request.method == "GET":
+        questions = Question.objects.all().values()
+        return JsonResponse(
+            {"code": 200, "message": "success", "data": list(questions)}
+        )
     return HttpResponse("Hello, world. You're at the polls index.")
 
 
@@ -18,3 +27,74 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+
+@csrf_exempt
+def create(request):
+    if request.method == "POST":
+        try:
+            form_data = json.loads(request.body.decode("utf-8"))
+            question = Question.objects.create(
+                question_text=form_data["question_text"],
+                pub_date=timezone.now(),
+                updated_date=timezone.now(),
+            )
+            return JsonResponse({"code": 200, "message": "success", "data": None})
+        except Exception as e:
+            return JsonResponse({"code": 500, "message": str(e), "data": None})
+    return JsonResponse({"code": 405, "message": "Method Not Allowed", "data": None})
+
+
+@csrf_exempt
+def get_poll(request):
+    id = request.GET.get("id")
+    if request.method == "GET":
+        try:
+            question = Question.objects.filter(id=id).values()
+            return JsonResponse(
+                {"code": 200, "message": "success", "data": list(question)}
+            )
+        except Exception as e:
+            return JsonResponse({"code": 500, "message": str(e), "data": None})
+    return JsonResponse({"code": 405, "message": "Method Not Allowed", "data": None})
+
+
+@csrf_exempt
+def get_poll_list(request):
+    if request.method == "GET":
+        try:
+            questions = Question.objects.all().values()
+            return JsonResponse(
+                {"code": 200, "message": "success", "data": list(questions)}
+            )
+        except Exception as e:
+            return JsonResponse({"code": 500, "message": str(e), "data": None})
+    return JsonResponse({"code": 405, "message": "Method Not Allowed", "data": None})
+
+
+@csrf_exempt
+def delete_poll(request):
+    if request.method == "DELETE":
+        try:
+            id = request.GET.get("id")
+            Question.objects.filter(id=id).delete()
+            return JsonResponse({"code": 200, "message": "success", "data": None})
+        except Exception as e:
+            return JsonResponse({"code": 500, "message": str(e), "data": None})
+    return JsonResponse({"code": 405, "message": "Method Not Allowed", "data": None})
+
+
+@csrf_exempt
+def update_poll(request):
+    if request.method == "PUT":
+        try:
+            form_data = json.loads(request.body.decode("utf-8"))
+            print(form_data)
+            Question.objects.filter(id=form_data["id"]).update(
+                question_text=form_data["question_text"],
+                updated_date=timezone.now(),
+            )
+            return JsonResponse({"code": 200, "message": "success", "data": None})
+        except Exception as e:
+            return JsonResponse({"code": 500, "message": str(e), "data": None})
+    return JsonResponse({"code": 405, "message": "Method Not Allowed", "data": None})
