70846893ada2e09bde521a200596e5bf3d08e576
鲨鱼辣椒 <monishine_code@163.com>
2025-05-16 09:16:29 +0800

    feat: 创建子应用、调整子应用目录结构

--- backend/settings.py
+++ backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "backend.app.polls",
+    "backend.app.user",
 ]
 
 MIDDLEWARE = [
@@ -73,9 +75,17 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "devuser",
+        "PASSWORD": "devuser",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
