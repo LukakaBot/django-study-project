09f45e168b27532d6e58300e1f3ac27b93812c51
鲨鱼辣椒 <monishine_code@163.com>
2025-05-16 13:38:41 +0800

    feat: 连接数据库、创建模型

--- backend/settings.py
+++ backend/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "backend.app.polls.apps.PollsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "backend.app.polls",
+    # "backend.app.polls",
     "backend.app.user",
 ]
 
