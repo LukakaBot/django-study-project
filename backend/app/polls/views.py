c6652a0d422477281cfb9e67a4cf6180ed3bf55f
鲨鱼辣椒 <monishine_code@163.com>
2025-05-16 16:36:30 +0800

    feat: commit

--- backend/app/polls/views.py
+++ backend/app/polls/views.py
@@ -4,13 +4,18 @@ from django.http import HttpResponse, JsonResponse
 from .models import Question
 from django.utils import timezone
 from django.views.decorators.csrf import csrf_exempt
+from django.template import loader
 
 
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = "，".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    # output = "，".join([q.question_text for q in latest_question_list])
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
